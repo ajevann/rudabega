e122a2301c5aaa240a9005dc35166b034ddb87db
Alex Vann <ajevann@gmail.com>
2014-11-23 12:09:07 -0500

    New versions.

--- testing/app.js
+++ testing/app.js
@@ -1,13 +1,29 @@
 angular.module('galleryApp',[]).
-    factory('datasource', ['$http',function($http){
-       return {
-           get: function(fileName,callback){
-                $http.get(fileName).success(function(data, status) {
-                    callback(data);
-                });
-           }
-       };
-    }]);
+  factory('datasource', ['$http',function($http){
+    return {
+      get: function(fileName,callback){
+        $http.get(fileName).success(function(data, status) {
+          callback(data);
+        });
+      }
+    };
+  }])
+  .controller('projectcontroller', ['$scope', function($scope) {
+    $scope.show = function($ id) {
+      $scope.currentImage = id;
+      $scope.emit('showProject');
+    };
+  }])
+  .directive('projectdirective', function() {
+    return {
+      replace: true,
+      link : function (scope, element, attrs) {
+        scope.$on('showProject', function() {
+          console.log('show');
+        });
+      },
+    };
+  });
 
 var gallerycontroller = function($scope,datasource) {
     var IMAGE_WIDTH = 405;
